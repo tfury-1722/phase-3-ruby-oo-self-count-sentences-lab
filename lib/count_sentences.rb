f999a57ec50ef29f98e197858a43f2412a4b1b72
christopher <cdnance1829@gmail.com>
2022-07-28 18:23:09 -0400

    Completed Assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,35 @@
-require 'pry'
+#require 'pry'
 
 class String
 
   def sentence?
-    
+    if(self.end_with?('.'))
+      return true
+    else
+      false
+    end
   end
 
   def question?
-
+    if(self.end_with?('?'))
+      return true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if(self.end_with?('!'))
+      return true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    #total_strings = 0
+    sentences_list = self.split(/[$.?!]\s/)
+    #sentences_list.each{|phrase| total_strings += 1}
+    sentences_list.length
   end
 end
